e8f0afa66a3c5dc2127a17d7364f1ddb812d65ff
Comp <withoutme1988@gmail.com>
2021-09-09 19:27:36 +0300

    PY-200 practice 2

--- Атрибуты и методы/Практические задания/task1_LinkedList_init_linked_list/main.py
+++ Атрибуты и методы/Практические задания/task1_LinkedList_init_linked_list/main.py
@@ -14,10 +14,11 @@ class LinkedList:
         """ Метод, который создает вспомогательный список и связывает в нём узлы. """
         self.list_nodes = [Node(value) for value in data]
 
-        # TODO составьте алгоритм, который свяжет узлы
+        for i in range(len(self.list_nodes) - 1):
+            self.linked_nodes(self.list_nodes[i], self.list_nodes[i+1])
 
-    # TODO каким должен быть этот метод?
-    def linked_nodes(self, left_node: Node, right_node: Optional[Node] = None) -> None:
+    @staticmethod
+    def linked_nodes(left_node: Node, right_node: Optional[Node] = None) -> None:
         """
         Функция, которая связывает между собой два узла.
 
